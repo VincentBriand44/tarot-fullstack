0382b8620b996b0db4e3007efe3b590c8a1d4b34
VincentBriand44 <vincent-briand_student2022@wilder.school>
2023-04-11 18:05:31 +0200

    :truck: move home modules in specific folder

---
+++ src/lib/components/home/PlayerRanking.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Player } from '$types/app';
+
+	export let players: Player[];
+</script>
+
+<section class="flex flex-col items-center w-64 p-4 pb-6 rounded-xl bg-slate-900">
+	<h2 class="mb-2 text-lg font-bold">Classement</h2>
+	<ul class="w-full">
+		{#each players as { name, score }}
+			<li class="flex justify-between">{name}<span class="text-slate-400">{score}</span></li>
+		{/each}
+	</ul>
+</section>
