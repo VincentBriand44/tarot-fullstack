d1bee44c105eb2eb0752ed89b2ccee3f9c23cbcb
VincentBriand44 <vincent-briand_student2022@wilder.school>
2023-04-24 23:45:39 +0200

    :construction: calendar page

--- src/lib/types/app.d.ts
+++ src/lib/types/app.d.ts
@@ -4,3 +4,24 @@ export interface Nav {
 	icon: string | undefined;
 	active: boolean;
 }
+
+interface Day {
+	name: string;
+	enabled: boolean;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	date: any;
+}
+
+interface Item {
+	title: string;
+	subtitle?: string;
+	detailHeader?: string;
+	detailContent?: string;
+	className: string;
+	startCol?: number;
+	date: Date;
+	len: number;
+	vlen?: number;
+	startRow?: number;
+	isBottom?: boolean;
+}
